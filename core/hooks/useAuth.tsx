15a6ee0ad6b15cd168040651dce8c0bdb7c7004f
Remco <57683378+remcostoeten@users.noreply.github.com>
2024-06-12 07:46:15 +0200

    Update index.ts

--- core/hooks/useAuth.tsx
+++
@@ -1,8 +0,0 @@
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from '@/core/database/firebase'; // Replace with the path to your firebase.ts
-
-export function useAuth() {
-    const [user, loading, error] = useAuthState(auth);
-
-    return { user, loading, error };
-}
